96eb816f477ad0df9226d5529fc101f2f96620d2
Rowan-Paul <rpflynn80@gmail.com>
2021-03-16 17:50:49 +0100

    Add route to get all tracks not in a playlist yet

--- spotitube/src/main/java/com/rowanpaulflynn/service/TrackService.java
+++ spotitube/src/main/java/com/rowanpaulflynn/service/TrackService.java
@@ -1,13 +1,13 @@
 package com.rowanpaulflynn.service;
 
+import com.rowanpaulflynn.dao.IPlaylistDAO;
 import com.rowanpaulflynn.dao.ITrackDAO;
-import com.rowanpaulflynn.service.dto.TracksListDTO;
+import com.rowanpaulflynn.dao.IUserDAO;
+import com.rowanpaulflynn.domain.Track;
+import com.rowanpaulflynn.domain.User;
 
 import javax.inject.Inject;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -17,28 +17,48 @@ import java.util.ArrayList;
 @Path("/tracks")
 public class TrackService {
 
+    IUserDAO userDAO;
+    IPlaylistDAO playlistDAO;
     private ITrackDAO trackDAO;
 
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getTracks() {
-        // Retrieve user from database
-        ArrayList tracks = trackDAO.getTracks();
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getTracks(@QueryParam("token") String token, @QueryParam("forPlaylist") int playlistid) {
+        User user = userDAO.verifyToken(token);
 
-        if (tracks.size() < 1) {
-            return Response.status(404).build();
+        ArrayList<Track> inPlaylist = playlistDAO.getTracksFromPlaylist(playlistid);
+        ArrayList<Track> allTracks = trackDAO.getTracks();
+
+        if (playlistid > 0 && inPlaylist.size() > 0) {
+            ArrayList<Track> tracks = new ArrayList<>();
+
+            allTracks.forEach((track) -> {
+                inPlaylist.forEach((playlistTrack) -> {
+                    if (playlistTrack.getId() == track.getId()) {
+                        tracks.add(playlistDAO.getTrackInfo(track.getId()));
+                    }
+                });
+            });
+            return Response.status(200).entity(tracks).build();
+        } else {
+            return Response.status(200).entity(allTracks).build();
         }
-
-        TracksListDTO tracksListDTO = new TracksListDTO();
-        tracksListDTO.tracks = tracks;
-
-
-        return Response.status(200).entity(tracksListDTO).build();
     }
 
     @Inject
     public void setTrackDAO(ITrackDAO trackDAO) {
         this.trackDAO = trackDAO;
+    }
+
+    @Inject
+    public void setUserDAO(IUserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Inject
+    public void setPlaylistDAO(IPlaylistDAO playlistDAO) {
+        this.playlistDAO = playlistDAO;
     }
 }
